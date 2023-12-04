250e4f6a551453f3cac9c3dd4a90cb96e318c1c2
ewbest <150290693+ewbest@users.noreply.github.com>
2023-12-04 16:21:22 -0500

    Create codeRoughDraft.R

---
+++ codeRoughDraft.R
@@ -0,0 +1,14 @@
+#Raw R Code for our data exploration questions:
+
+#----------------------------
+#-Data Exploration Questions-
+#----------------------------
+#1. How common are charging stations? 
+#   Answer: We will use the study in (4) to create a map of charging stations and then analyze the data
+
+#2. What vehicle has the longest excursion time/length on a full-charge? 
+#   Answer: We will use the study in (3) to analyze this data. Note on electric vehicles:
+#   Weather can affect their performance, especially when it's cold.
+
+#3. Which vehicles are the most cost-efficient when compared with traditional ICE cars? 
+#   Answer: We must find a dataset which discusses these economic factors.
