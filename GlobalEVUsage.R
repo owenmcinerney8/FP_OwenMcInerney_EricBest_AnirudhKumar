0f18cee594769834867b5de7212aa5c1977419bb
owen8m <144965255+owenmcinerney8@users.noreply.github.com>
2023-12-07 19:42:23 -0500

    wrangled IEA EV dataset
    next step: create histogram

--- GlobalEVUsage.R
+++ GlobalEVUsage.R
@@ -1,3 +1,24 @@
+## Use groundhog to make sure the code runs mostly everywhere
+library(groundhog)
+groundhog.day="2023-11-20"
+
+pkgs=c('here', 'dplyr', 'dbplyr', 'ggplot2')
+groundhog.library(pkgs, groundhog.day)
+
 ## Using 'here' for a relative filepath
-csv_path <- here(".csv")
-EVfuelstations <- read.csv(csv_path)
+csv_path <- here("IEA_EV_SalesHistoricalCars.csv")
+EV_Global_Historical_raw <- read.csv(csv_path)
+
+## wrangled data to cases of groups of cars with attributes: year, region, count
+EV_production_region_year <- EV_Global_Historical_raw %>%
+  filter(unit == 'Vehicles', mode == 'Cars', region != "World") %>%
+  select(region, year, value) %>%
+  group_by(region, year) %>%
+  summarise(count = sum(value))
+
+## wrangled data to cases of groups of cars with attributes: year, count
+EV_production_global_year <- EV_production_region_year %>%
+  group_by(year) %>%
+  summarise(count = sum(count))
+
+## NEXT STEP :: CREATE HISTOGRAM
