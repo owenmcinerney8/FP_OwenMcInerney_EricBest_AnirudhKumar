5bffa4053e8719591023c4ae6c51ad800d8c6ec8
owen8m <144965255+owenmcinerney8@users.noreply.github.com>
2023-12-08 10:59:59 -0500

    new comments, polished data visualization

--- Q4_5GlobalEVUsage.R
+++ Q4_5GlobalEVUsage.R
@@ -2,18 +2,15 @@
 ## Use groundhog to make sure the code runs mostly everywhere
 library(groundhog)
 groundhog.day="2023-11-20"
-
 pkgs=c('here', 'dplyr', 'dbplyr', 'ggplot2')
 groundhog.library(pkgs, groundhog.day)
 
+
 ### Load Data
 ## Using 'here' for a relative filepath
-options(scipen = 999)
-
+options(scipen = 999) # Ensures scienfic notation is not used
 csv_path <- here("IEA_EV_SalesHistoricalCars.csv")
 EV_Global_Historical_raw <- read.csv(csv_path)
-
-
 
 
 ### Wrangle Data
@@ -27,33 +24,42 @@ EV_production_region_year <- EV_Global_Historical_raw %>%
 ## wrangled data to cases of groups of cars with attributes: year, count
 EV_production_global_year <- EV_production_region_year %>%
   group_by(year) %>%
-  summarise(count = sum(count))
+  summarise(count = sum(count)) 
 
 
-### CREATE HISTOGRAM
-ggplot(EV_production_global_year, aes(x = year)) +
-  geom_histogram(bins = 13)
-
-ggplot(EV_production_global_year, aes(x = year, y = count, fill = 'red')) +
+### CREATE Bar Chart of Global Electric Car Ownership by Year
+global_ev_ownership_by_year_bar_chart <- ggplot(EV_production_global_year, aes(x = year, y = count, fill = 'red')) +
   geom_col() + 
+  scale_y_continuous(n.breaks = 8, limits = c(0, max(EV_production_global_year$count))) +
+  scale_x_continuous(n.breaks = 11) +
   labs(title = "Global Electirc Cars in Use Since 2010",
        x = "Year",
        y = "Cars") +
   theme_classic() +
-  theme(legend.position = "none")
+  theme(legend.position = "none") # removes legend
 
-### Create Summary Table of Top 5 Leading Countries in 
+global_ev_ownership_by_year_bar_chart # to view bar chart
+
+### Car bar chart of 
+# Wrangle Data to get total sum of car ownership by region between 2019 and 2022
 EV_usage_region_2019_2022 <- EV_production_region_year %>%
   filter(year >= 2019) %>%
   group_by(region) %>%
   summarise(count = sum(count))
 
-EV_ownership_order_by_country <- EV_usage_region_2019_2022[order(EV_usage_region_2019_2022$count, decreasing = TRUE),]
-
-ggplot(EV_ownership_order_by_country, aes(x = region, y = count, fill = 'red')) +
+# use y = reorder(region, +count) to order countries by car ownership
+regional_ev_ownership_bar_chart <- ggplot(EV_ownership_order_by_country, aes(x = count, y = reorder(region, +count), fill = 'red')) +
   geom_col() + 
-  labs(title = "Electric Car Ownership By Country",
-       x = "Country",
-       y = "Cars") +
+  scale_x_continuous(limits = c(0, max(EV_ownership_order_by_country$count)), n.breaks = 9) +
+  labs(title = "Electric Car Ownership By Region",
+       subtitle = "Total Cars Owned from 2019 to 2022 for each Region",
+       x = "Cars",
+       y = "Region") +
   theme_classic() +
-  theme(legend.position = "none")
+  theme(legend.position = "none") # removes legend
+
+regional_ev_ownership_bar_chart # to view bar chart
+
+### View Charts
+global_ev_ownership_by_year_bar_chart
+regional_ev_ownership_bar_chart
