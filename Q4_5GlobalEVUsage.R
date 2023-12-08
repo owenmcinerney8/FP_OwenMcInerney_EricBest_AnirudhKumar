10a342ac4d25c26e90c1c2ca877e0fc518a2e3bf
owen8m <144965255+owenmcinerney8@users.noreply.github.com>
2023-12-08 12:13:31 -0500

    replaced IEA tidying code image

--- Q4_5GlobalEVUsage.R
+++ Q4_5GlobalEVUsage.R
@@ -7,22 +7,22 @@ groundhog.library(pkgs, groundhog.day)
 
 
 ### Load Data
-## Using 'here' for a relative filepath
-options(scipen = 999) # Ensures scienfic notation is not used
+## Using 'here' for a relative file path
+options(scipen = 999) # Ensures scientific notation is not used
 csv_path <- here("IEA_EV_SalesHistoricalCars.csv")
 EV_Global_Historical_raw <- read.csv(csv_path)
 
 
 ### Wrangle Data
-## tidy data to cases of groups of cars with attributes: year, region, count
-EV_production_region_year <- EV_Global_Historical_raw %>%
-  filter(unit == 'Vehicles', mode == 'Cars', region != "World") %>%
+## wrangle data to include columns for: year, region, count of EVs
+EV_ownership_region_year_raw <- EV_Global_Historical_raw %>%
+  filter(unit == 'Vehicles', region != "World") %>%
   select(region, year, value) %>%
   group_by(region, year) %>%
   summarise(count = sum(value))
 
 ## wrangled data to cases of groups of cars with attributes: year, count
-EV_production_global_year <- EV_production_region_year %>%
+EV_production_global_year <- EV_ownership_region_year_raw %>%
   group_by(year) %>%
   summarise(count = sum(count)) 
 
@@ -32,9 +32,9 @@ global_ev_ownership_by_year_bar_chart <- ggplot(EV_production_global_year, aes(x
   geom_col() + 
   scale_y_continuous(n.breaks = 8, limits = c(0, max(EV_production_global_year$count))) +
   scale_x_continuous(n.breaks = 11) +
-  labs(title = "Global Electirc Car Ownership Since 2010",
+  labs(title = "Global EV Ownership Since 2010",
        x = "Year",
-       y = "Cars") +
+       y = "Count of Electric Vehicles") +
   theme_classic() +
   theme(legend.position = "none") # removes legend
 
@@ -51,9 +51,9 @@ EV_usage_region_2019_2022 <- EV_production_region_year %>%
 regional_ev_ownership_bar_chart <- ggplot(EV_ownership_order_by_country, aes(x = count, y = reorder(region, +count), fill = 'red')) +
   geom_col() + 
   scale_x_continuous(limits = c(0, max(EV_ownership_order_by_country$count)), n.breaks = 9) +
-  labs(title = "Electric Car Ownership By Region",
-       subtitle = "Total Cars Owned from 2019 to 2022 for each Region",
-       x = "Cars",
+  labs(title = "EV Ownership By Region",
+       subtitle = "Total EVs Owned from 2019 to 2022 for each Region",
+       x = "Count of Electric Vehicles",
        y = "Region") +
   theme_classic() +
   theme(legend.position = "none") # removes legend
