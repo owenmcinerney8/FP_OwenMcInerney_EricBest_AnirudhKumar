393aab8870d38da7f3e729a26bf77d84356eb5a6
owen8m <144965255+owenmcinerney8@users.noreply.github.com>
2023-12-10 14:26:14 -0500

    added q6 and fixed styling of code

--- Q4_5GlobalEVUsage.R
+++ Q4_5GlobalEVUsage.R
@@ -1,22 +1,22 @@
-### Load Packages
+### Load Packages --------------------------------------------------------------
 ## Use groundhog to make sure the code runs mostly everywhere
 library(groundhog)
-groundhog.day="2023-11-20"
-pkgs=c('here', 'dplyr', 'dbplyr', 'ggplot2')
+groundhog.day <- "2023-11-20"
+pkgs <- c("here", "dplyr", "dbplyr", "ggplot2")
 groundhog.library(pkgs, groundhog.day)
 
 
-### Load Data
+### Load Data ------------------------------------------------------------------
 ## Using 'here' for a relative file path
 options(scipen = 999) # Ensures scientific notation is not used
 csv_path <- here("IEA_EV_SalesHistoricalCars.csv")
 EV_Global_Historical_raw <- read.csv(csv_path)
 
 
-### Wrangle Data
+### Wrangle Data ---------------------------------------------------------------
 ## wrangle data to include columns for: year, region, count of EVs
 EV_ownership_region_year_raw <- EV_Global_Historical_raw %>%
-  filter(unit == 'Vehicles', region != "World") %>%
+  filter(unit == "Vehicles", region != "World") %>%
   select(region, year, value) %>%
   group_by(region, year) %>%
   summarise(count = sum(value))
@@ -24,23 +24,24 @@ EV_ownership_region_year_raw <- EV_Global_Historical_raw %>%
 ## wrangled data to cases of years with attributes: year, count of EVs
 EV_ownership_global_year <- EV_ownership_region_year_raw %>%
   group_by(year) %>%
-  summarise(count = sum(count)) 
+  summarise(count = sum(count))
 
-
-### CREATE Bar Chart of Global EV Ownership by Year
-global_ev_ownership_by_year_bar_chart <- ggplot(EV_ownership_global_year, aes(x = year, y = count, fill = 'red')) +
-  geom_col() + 
+### CREATE Bar Chart of Global EV Ownership by Year ----------------------------
+global_ev_ownership_by_year_bar_chart <- ggplot(EV_ownership_global_year, aes(x = year, y = count, fill = "red")) +
+  geom_col() +
   scale_y_continuous(n.breaks = 8, limits = c(0, max(EV_production_global_year$count))) +
   scale_x_continuous(n.breaks = 11) +
-  labs(title = "Global EV Ownership since 2010",
-       x = "Year",
-       y = "Count of Electric Vehicles") +
+  labs(
+    title = "Global EV Ownership since 2010",
+    x = "Year",
+    y = "Count of Electric Vehicles"
+  ) +
   theme_classic() +
   theme(legend.position = "none") # removes legend
 
 global_ev_ownership_by_year_bar_chart # to view bar chart
 
-### Car bar chart of 
+### Car bar chart of EV Ownership by Country -----------------------------------
 # Wrangle Data to get cases of regions from 2019-2022 with attributes: region, count
 EV_ownership_region_2019_2022 <- EV_ownership_region_year_raw %>%
   filter(year >= 2019) %>%
@@ -48,40 +49,46 @@ EV_ownership_region_2019_2022 <- EV_ownership_region_year_raw %>%
   summarise(count = sum(count))
 
 # use y = reorder(region, +count) to order countries by car ownership
-regional_ev_ownership_bar_chart <- ggplot(EV_ownership_region_2019_2022, aes(x = count, y = reorder(region, +count), fill = 'red')) +
-  geom_col() + 
+regional_ev_ownership_bar_chart <- ggplot(EV_ownership_region_2019_2022, aes(x = count, y = reorder(region, +count), fill = "red")) +
+  geom_col() +
   scale_x_continuous(limits = c(0, max(EV_ownership_order_by_country$count)), n.breaks = 6) +
-  labs(title = "EV Ownership By Region",
-       subtitle = "Total EVs Owned from 2019 to 2022 for each Region",
-       x = "Count of Electric Vehicles",
-       y = "Region") +
+  labs(
+    title = "EV Ownership By Region",
+    subtitle = "Total EVs Owned from 2019 to 2022 for each Region",
+    x = "Count of Electric Vehicles",
+    y = "Region"
+  ) +
   theme_classic() +
   theme(legend.position = "none") # removes legend
 
 regional_ev_ownership_bar_chart # to view bar chart
 
-### View Charts
-global_ev_ownership_by_year_bar_chart
-regional_ev_ownership_bar_chart
-
 ### Q5 Expansion: What percentage of EVs do the top 3 owning EV countries own?
-EV_ownership_region_2019_2022_order_count <- EV_ownership_region_2019_2022[order(EV_ownership_region_2019_2022$count, decreasing = TRUE),]
 
-top_3_count = (sum(EV_ownership_region_2019_2022_order_count[1:3,2])/sum(EV_ownership_region_2019_2022_order_count$count)) * 100
-bottom_count = sum(EV_ownership_region_2019_2022_order_count[4:nrow(EV_ownership_region_2019_2022_order_count),2])/sum(EV_ownership_region_2019_2022_order_count$count) * 100
+## Wrangle Data to top 3 (China, Europe, EU) vs everyone else
+EV_ownership_region_2019_2022_order_count <- EV_ownership_region_2019_2022[order(EV_ownership_region_2019_2022$count, decreasing = TRUE), ]
+
+top_3_count <- (sum(EV_ownership_region_2019_2022_order_count[1:3, 2]) / sum(EV_ownership_region_2019_2022_order_count$count)) * 100
+bottom_count <- sum(EV_ownership_region_2019_2022_order_count[4:nrow(EV_ownership_region_2019_2022_order_count), 2]) / sum(EV_ownership_region_2019_2022_order_count$count) * 100
 
 top_3_count
 bottom_count
 
-top_vs_bottom_df <- data.frame(Top3 = c('Yes', 'No'), percent = c(top_3_count, bottom_count))
+top_vs_bottom_df <- data.frame(Top3 = c("Yes", "No"), percent = c(top_3_count, bottom_count)) # create data frame
 
+## Create Bar Chart of Top 3 vs Bottom 3
 top_vs_bottom_percent <- top_vs_bottom_df %>%
   ggplot(aes(x = Top3, y = percent, fill = Top3)) +
   geom_col() +
-  geom_text(aes(label = round(percent), y = percent + 2), size = 3) +
-  labs(title = "Percent of World's EVs Owned", subtitle = "Leading 3 Regions Vs The Rest of the World",
-       x = "Top 3", y = "Percentage") +
-  scale_y_continuous(n.breaks = 10, limits = c(0,100)) +
+  geom_text(aes(label = paste(as.character(round(percent, digits = 2)), "%", sep = ""), y = percent + 2), size = 3) +
+  labs(
+    title = "Percent of World's EVs Owned", subtitle = "Leading 3 Regions Vs The Rest of the World",
+    x = "Top 3", y = "Percentage"
+  ) +
+  scale_y_continuous(n.breaks = 10, limits = c(0, 100)) +
   theme_bw()
 
+### View Charts ----------------------------------------------------------------
+global_ev_ownership_by_year_bar_chart
+regional_ev_ownership_bar_chart
 top_vs_bottom_percent
--- Q6MPG_EV_vs _GAS.R
+++ Q6MPG_EV_vs _GAS.R
@@ -1,65 +1,69 @@
-### Load Packages
+### Load Packages --------------------------------------------------------------
 ## Use groundhog to make sure the code runs mostly everywhere
 library(groundhog)
-groundhog.day="2023-11-20"
-pkgs=c('readxl', 'dplyr', 'dbplyr', 'ggplot2', 'tidyverse', 'knitr', 'kableExtra')
+groundhog.day <- "2023-11-20"
+pkgs <- c("readxl", "dplyr", "dbplyr", "ggplot2", "tidyverse", "knitr", "kableExtra")
 groundhog.library(pkgs, groundhog.day)
 
-### read in data
+### read in data ---------------------------------------------------------------
 EV_GAS_data_raw <- read_excel("all_alpha_24.xlsx")
 
 ### Wrangle data to include Model, fuel, veh_class, air pollution score, city mpg, highway mpg, greenhouse gas score
 
 # Change names of columns with spaces
-colnames(EV_GAS_data_raw)[11] = "veh_class"
-colnames(EV_GAS_data_raw)[12] = "air_pollution_score"
-colnames(EV_GAS_data_raw)[13] = "city_mpg"
-colnames(EV_GAS_data_raw)[14] = "highway_mpg"
-colnames(EV_GAS_data_raw)[16] = "greenhouse_gas_score"
+colnames(EV_GAS_data_raw)[11] <- "veh_class"
+colnames(EV_GAS_data_raw)[12] <- "air_pollution_score"
+colnames(EV_GAS_data_raw)[13] <- "city_mpge"
+colnames(EV_GAS_data_raw)[14] <- "highway_mpge"
+colnames(EV_GAS_data_raw)[16] <- "greenhouse_gas_score"
 
 # Select to Model, fuel, veh_class, air pollution score, city mpg, highway mpg, greenhouse gas score
 EV_GAS_data <- EV_GAS_data_raw %>%
-  drop_na(city_mpg) %>%
+  drop_na(city_mpge) %>%
   filter(Fuel == "Gasoline" | Fuel == "Electricity") %>%
-  select(Model, Fuel, veh_class, air_pollution_score, city_mpg, highway_mpg, greenhouse_gas_score) %>%
-  mutate(city_mpg = as.numeric(city_mpg),
-         highway_mpg = as.numeric(highway_mpg),
-         greenhouse_gas_score = as.numeric(greenhouse_gas_score))
+  select(Model, Fuel, veh_class, air_pollution_score, city_mpge, highway_mpge, greenhouse_gas_score) %>%
+  mutate(
+    city_mpge = as.numeric(city_mpge),
+    highway_mpge = as.numeric(highway_mpge),
+    greenhouse_gas_score = as.numeric(greenhouse_gas_score)
+  )
 
-### Make Box Plots / Greenhouse Summary Table
-diamonds
-
-# City MPG Box Plot
-city_mpg_box_plot <- EV_GAS_data %>% 
-  group_by(Fuel) %>% 
-  ggplot(aes(x = Fuel, y = city_mpg, group = Fuel, fill = Fuel)) +
-  labs(title = "City MPG EV vs Gas Box Plot", subtitle = "EV EPA converted to MPG", x = "Fuel", y = "MPG") + 
-  scale_y_continuous(n.breaks = 10, limits = c(0, max(EV_GAS_data$city_mpg))) +
-  geom_boxplot(outlier.colour="black", outlier.shape=16,
-               outlier.size=2, notch=FALSE) +
+### Make Box Plots / Greenhouse Summary Table ----------------------------------
+## City MPG Box Plot
+city_mpge_box_plot <- EV_GAS_data %>%
+  group_by(Fuel) %>%
+  ggplot(aes(x = Fuel, y = city_mpge, group = Fuel, fill = Fuel)) +
+  labs(title = "City MPGe EV vs Gas Box Plot", subtitle = "MPGe is Miles per Gallon equivalent", x = "Fuel", y = "MPGe") +
+  scale_y_continuous(n.breaks = 10, limits = c(0, max(EV_GAS_data$city_mpge))) +
+  geom_boxplot(
+    outlier.colour = "black", outlier.shape = 16,
+    outlier.size = 2, notch = FALSE
+  ) +
   theme_minimal()
 
-# Highway MPG Box Plot
-highway_mpg_box_plot <- EV_GAS_data %>% 
-  group_by(Fuel) %>% 
-  ggplot(aes(x = Fuel, y = highway_mpg, group = Fuel, fill = Fuel)) +
-  labs(title = "Highway MPG EV vs Gas Box Plot", subtitle = "EV EPA converted to MPG", x = "Fuel", y = "MPG") + 
-  scale_y_continuous(n.breaks = 10, limits = c(0, max(EV_GAS_data$highway_mpg))) +
-  geom_boxplot(outlier.colour="black", outlier.shape=16,
-               outlier.size=2, notch=FALSE) +
+## Highway MPG Box Plot
+highway_mpge_box_plot <- EV_GAS_data %>%
+  group_by(Fuel) %>%
+  ggplot(aes(x = Fuel, y = highway_mpge, group = Fuel, fill = Fuel)) +
+  labs(title = "Highway MPGe EV vs Gas Box Plot", subtitle = "MPGe is Miles per Gallon equivalent", x = "Fuel", y = "MPGe") +
+  scale_y_continuous(n.breaks = 10, limits = c(0, max(EV_GAS_data$highway_mpge))) +
+  geom_boxplot(
+    outlier.colour = "black", outlier.shape = 16,
+    outlier.size = 2, notch = FALSE
+  ) +
   theme_minimal()
 
-# Greenhouse Gas Score Summary Table
+## Greenhouse Gas Score Summary Table
 funx <- list(
-  min = ~min(.x, na.rm = TRUE),
-  Q1 = ~quantile(.x, probs = 0.20, na.rm = TRUE),
-  Q2 = ~quantile(.x, probs = 0.40, na.rm = TRUE),
-  median = ~median(.x, na.rm = TRUE),
-  Q3 = ~quantile(.x, probs = 0.60, na.rm = TRUE),
-  Q4 = ~quantile(.x, probs = 0.80, na.rm = TRUE),
-  max = ~max(.x, na.rm = TRUE),
-  mean = ~mean(.x, na.rm = TRUE),
-  std = ~sd(.x, na.rm = TRUE)
+  min = ~ min(.x, na.rm = TRUE),
+  Q1 = ~ quantile(.x, probs = 0.20, na.rm = TRUE),
+  Q2 = ~ quantile(.x, probs = 0.40, na.rm = TRUE),
+  median = ~ median(.x, na.rm = TRUE),
+  Q3 = ~ quantile(.x, probs = 0.60, na.rm = TRUE),
+  Q4 = ~ quantile(.x, probs = 0.80, na.rm = TRUE),
+  max = ~ max(.x, na.rm = TRUE),
+  mean = ~ mean(.x, na.rm = TRUE),
+  std = ~ sd(.x, na.rm = TRUE)
 )
 
 green_house_gas_data <- EV_GAS_data %>%
@@ -67,17 +71,18 @@ green_house_gas_data <- EV_GAS_data %>%
   select(Fuel, greenhouse_gas_score) %>%
   summarize(
     across(
-      .cols=where(is.numeric),
-      .fns = funx),
-    count =n()
+      .cols = where(is.numeric),
+      .fns = funx
+    ),
+    count = n()
   )
 
 greenhouse_gas_score_summary_table <- green_house_gas_data %>%
   kable() %>%
   kable_classic()
 
-# View Data Visualizations
-city_mpg_box_plot
-highway_mpg_box_plot
+### View Data Visualizations ---------------------------------------------------
+city_mpge_box_plot
+highway_mpge_box_plot
 greenhouse_gas_score_summary_table
 
